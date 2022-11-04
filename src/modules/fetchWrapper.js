9d4683fc38e6a4948b48beb0d152d5df7e6de57c
UmerZia <omerzia73@gmail.com>
2022-11-04 20:21:09 +0500

    Fix linter error

--- src/modules/fetchWrapper.js
+++ src/modules/fetchWrapper.js
@@ -1,6 +1,7 @@
 const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
 const involveKey = '9Il7nZFFQBX3bIEy06IW';
 
+// Generate Unique ID
 const getInvolveKey = async (url) => {
   const data = await fetch(baseUrl + url, {
     method: 'POST',
@@ -12,26 +13,31 @@ const getInvolveKey = async (url) => {
   return Data;
 };
 
+// Fetch Existing likes from API
 const allLikes = async () => {
+  // Fetch total likes from API
   const allLikes = await fetch(`${baseUrl + involveKey}/likes/`, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   });
   const AllLikes = await allLikes.json();
-  console.dir(AllLikes);
   return AllLikes;
 };
 
+// Add new like
 const like = async (id) => {
+  // Parameters
   const object = {
     item_id: id,
   };
+  // Update Likes
   await fetch(`${baseUrl + involveKey}/likes/`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(object),
   });
 
+  // Update Likes UI
   const likes = await allLikes();
   const likesOfMovie = likes.find((item) => item.item_id === id)?.likes ?? 0;
   const movie = document.getElementById(`likes-${id}`);
