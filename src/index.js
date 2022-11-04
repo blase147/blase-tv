b281e36956cae70eca6cc7e5feff6ba3d404539b
UmerZia <omerzia73@gmail.com>
2022-11-04 19:52:21 +0500

    Add update like feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './index.css';
 import listOfMovies from './modules/homepage.js';
+import api from './modules/fetchWrapper.js';
 
 const body = document.getElementById('body');
 body.onload = () => { window.listOfMovies = listOfMovies(); };
+
+window.likeitem = api.like;
--- src/modules/fetchWrapper.js
+++ src/modules/fetchWrapper.js
@@ -1,16 +1,46 @@
-const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
 const involveKey = '9Il7nZFFQBX3bIEy06IW';
 
 const getInvolveKey = async (url) => {
-  const data = await fetch(baseUrl+url,{
+  const data = await fetch(baseUrl + url, {
     method: 'POST',
     headers: {
       'Content-type': 'text/JSON; charset=utf-8',
     },
   });
-  console.dir(await data.text());
-  return await data.text();
-}
+  const Data = await data.text();
+  return Data;
+};
 
-const api = { getInvolveKey }
+const allLikes = async () => {
+  const allLikes = await fetch(`${baseUrl + involveKey}/likes/`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const AllLikes = await allLikes.json();
+  console.dir(AllLikes);
+  return AllLikes;
+};
+
+const like = async (id) => {
+  const object = {
+    item_id: id,
+  };
+  await fetch(`${baseUrl + involveKey}/likes/`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(object),
+  });
+
+  const likes = await allLikes();
+  const likesOfMovie = likes.find((item) => item.item_id === id)?.likes ?? 0;
+  const movie = document.getElementById(`likes-${id}`);
+  movie.innerText = likesOfMovie;
+};
+
+const api = {
+  getInvolveKey,
+  like,
+  allLikes,
+};
 export default api;
